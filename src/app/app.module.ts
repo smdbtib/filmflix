0340fac92d7831c8875e33424219d2a88fd756ec
smdbtib <smdb.tib@outlook.com>
2022-01-17 17:58:51 -0300

    Config firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'filmflix-5f072',
+    appId: '1:1043368710257:web:c654d3e3aef4d9ae3fcbd0',
+    storageBucket: 'filmflix-5f072.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDMTffC09uKMC8r3LdzCDSqdouB7uk2tHE',
+    authDomain: 'filmflix-5f072.firebaseapp.com',
+    messagingSenderId: '1043368710257',
+  },
   production: false
 };
 
